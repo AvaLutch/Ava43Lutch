8d5d914a21c9edd166f5967a3461c8ee1feab62f
AvaLutch <link74101@gmail.com>
2026-05-10 18:55:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
   background: "https://images2.imgbox.com/f7/ca/It5Je5RF_o.jpg",
-  profile: "https://images2.imgbox.com/c6/f7/GSLgoKd0_o.jpg",
+  profile: "https://images2.imgbox.com/eb/ae/CH957Goz_o.jpg",
   whatsapp: "https://tapthelink.cfd/yQzJt",
   telegram: "https://tapthelink.cfd/yQzJt",
   minOnline: 700,
