a91210fa4f709c3b3114739bd7cfda4296fab0e3
AvaLutch <link74101@gmail.com>
2026-08-06 20:53:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
   background: "https://cdn.phototourl.com/free/2026-07-30-bc8a3c06-b3d5-4d42-b3bc-74ab17a65634.jpg",
-  profile: "https://cdn.phototourl.com/free/2026-07-30-fe65602b-9bec-4296-817a-168279ac1daf.jpg",
+  profile: "https://cdn.phototourl.com/free/2026-08-06-4f763aa0-91d2-4791-928e-39b7e54aba64.jpg",
   whatsapp: "https://tapthelink.cfd/yQzJt",
   telegram: "https://tapthelink.cfd/yQzJt",
   minOnline: 700,
